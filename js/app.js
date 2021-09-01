aadb59ae44af16962255e2e7477ddf60ff0dfa39
Sami un nobi samid <67954576+NobiSamid@users.noreply.github.com>
2021-09-02 04:30:51 +0600

    motamoti kichu hoise

--- js/app.js
+++ js/app.js
@@ -3,10 +3,14 @@ const getSearchedText = () =>{
     const searchText = getSearchField.value;
     getSearchField.value = ''
     console.log(searchText)
-    const url = `http://openlibrary.org/search.json?q=${searchText}`
+    const url = `HTTPS://openlibrary.org/search.json?q=${searchText}`
+
+    document.getElementById('spinner').classList.remove("d-none")
+    document.getElementById('update').classList.add("d-none")
+
     fetch(url)
     .then(res => res.json())
-    .then(data => displayApi(data))
+    .then(data => getApi(data))
     .catch(error => displayError(error));
 }
 
@@ -14,25 +18,45 @@ const displayError = error =>{
     console.log('jhamela hoise holo', error)
 }
 
-const displayApi = data => {
+const getApi = data => {
     console.log(data)
     console.log(data.numFound)
     console.log(data.docs[0].author_name[0])
     console.log(data.docs[0].text[1])
-    const displaySection = document.getElementById('update');
-    const div = document.createElement('div')
+
+    const bookArray = data.docs;
+
+    // console.log(bookArray);
+    // console.log(bookArray.filter( bookArray => bookArray.title !== 'Harry'))
+
+    const resultNumber = document.getElementById('result')
     const h5 = document.createElement('h5')
     h5.innerText = `Result found ${data.numFound}`
+    resultNumber.appendChild(h5);
+
+    const displaySection = document.getElementById('update');
+    displaySection.textContent='';
+    bookArray?.forEach(element => {
+        displayApi(element);        
+    });
+}
+const displayApi = element =>{
+    console.log(element)
+    const displaySection = document.getElementById('update');
+    const div = document.createElement('div')
+    div.classList.add('col')
     div.classList.add('card')
-    // div.innerText = `name of book ${data.docs[0].text[1]}`
     div.innerHTML = `
     <div>
-      <h1>${data.docs[0].text[1]}</h1>
-      <p>${data.docs[0].author_name[0]}</p>
+    <img src="HTTPS://covers.openlibrary.org/b/id/${element.cover_i}-L.jpg" alt="">
+      <h1>${element.title}</h1>
+      <p>${element.author_name[0] ? element.author_name[0] :''}</p>
+      <p>${element.publisher[0] ? element.publisher[0] : ''} </p>
+      <p>${element.publish_date[0] ? element.publish_date[0] : ''}</p>
     </div>
     `
-    displaySection.appendChild(h5);
     displaySection.appendChild(div);
-    
 
+    document.getElementById('update').classList.remove("d-none")
+    document.getElementById('spinner').classList.add("d-none")
 }
